d1ab27f6ca3861362966910ffd4c4cf7b1c5a714
arulxavier857 <arul_vmax@karunya.edu>
2025-01-26 22:07:51 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css';  
+import RegisterForm from './RegisterForm';
+
+function App() {
+  return (
+    <>
+      <div className='bg-gray-200 p-10'>
+      <h1 className='text-blue-500'>Welcome to Health Plus</h1>
+      <RegisterForm/>
+      </div>
+    </>
+  )
+}
+export default App
+

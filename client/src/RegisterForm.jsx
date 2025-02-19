7af66db55ff44102bca8b4f8e29256d3c037ea23
arulxavier857 <arul_vmax@karunya.edu>
2025-02-19 15:58:18 +0530

    Fourth commit

--- client/src/RegisterForm.jsx
+++ client/src/RegisterForm.jsx
@@ -1,10 +1,6 @@
 import React, { useState } from "react";
 
 function RegisterForm() {
-
-  const [name,setName] = useState()
-  const [email,setEmail] = useState()
-  const [phone,setPhone] = useState()
   return (
     <div className="w-full max-w-md mx-auto mt-20">
       <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 border">
